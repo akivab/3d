feaf833c7c0f23c81fa1d0dfdc2b3301c0f795a1
Akiva Bamberger <akiva.bamberger@gmail.com>
2013-12-30 07:09:44 -0800

    Making movies

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // Dependencies
 var express = require('express'),
     exphbs  = require('express3-handlebars'),
+    fs = require('fs'),
     path = require('path'),
+    sys = require('sys'),
     app = express();
 
 // Basic config
@@ -9,6 +11,7 @@ app.use(express.static(path.join('public')));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.bodyParser());
+app.use(express.multipart());
 app.use(express.methodOverride());
 
 // View config
@@ -22,6 +25,29 @@ app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 
 app.get('/', function(req, res){
   res.render('home');
+});
+
+app.post('/upload', function(req, res) {
+  console.log('body:');
+  console.log(req.body);
+  console.log('files:');
+  console.log(req.files);
+  // console.log(req.rawBody);
+  var folderName = 'public/uploads';
+  
+  var timestamp = req.body.timestamp;
+  var lat = req.body.lat;
+  var lng = req.body.long;
+
+  var fileName = 'movie-' + timestamp.replace(/\./g, '') + '.mov';
+
+
+  fs.mkdir(folderName, function(err){});
+  fs.readFile(req.files.video.path, function (err, data) {
+    var newPath = __dirname + '/' + folderName + '/' + fileName;
+    fs.writeFile(newPath, data, function (err) {});
+  });
+  res.send('done.');
 });
 
 app.listen(3000);
